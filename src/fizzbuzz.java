46013adf5a8fe595758a783f36e1adee7e1db520
Brian Martinez <marjosep@msudenver.edu>
2016-08-27 22:29:24 -0600

    added second method

--- src/fizzbuzz.java
+++ src/fizzbuzz.java
@@ -2,16 +2,34 @@ public class fizzbuzz
 {
     public static void main(String[] args) throws Exception
     {
-        for (int count = 1; count < 100; count++)
+        for (int i = 1; i <= 100; i++)
         {
-            if (count % 3 == 0)
-                System.out.print("fizz");
-            if (count % 5 == 0)
-                System.out.print("buzz");
-            if (count % 3 != 0 && count % 5 != 0)
-                System.out.print(count);
+            if (i % 3 == 0)
+                System.out.print("Fizz");
+            if (i % 5 == 0)
+                System.out.print("Buzz");
+            if (i % 3 != 0 && i % 5 != 0)
+                System.out.print(i);
 
             System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Second method: ");
+        fizzbuzz2();
+    }
+
+    public static void fizzbuzz2()
+    {
+        String fb;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            fb = (i % 3 == 0) ? "Fizz" : "";
+            fb += (i % 5 == 0) ? "Buzz" : "";
+            fb += ((i % 3 > 0) && (i % 5 > 0)) ? i : "";
+
+            System.out.println(fb);
         }
     }
 }
